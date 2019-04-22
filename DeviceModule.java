d73d5acf8b2b1df38470450e6238d5ea418a64fb
Tasnuva Tabassum oshin <tasnuva.oshin12@gmail.com>
2019-04-23 05:18:45 +0600

    Create DeviceModule.java

---
+++ DeviceModule.java
@@ -0,0 +1,30 @@
+public class DeviceModule {
+
+    public static String getMacAddress() {
+        try {
+            String interfaceName = "wlan0";
+            List<NetworkInterface> interfaces = Collections.list(NetworkInterface.getNetworkInterfaces());
+            for (NetworkInterface intf : interfaces) {
+                if (!intf.getName().equalsIgnoreCase(interfaceName)) {
+                    continue;
+                }
+
+                byte[] mac = intf.getHardwareAddress();
+                if (mac == null) {
+                    return "";
+                }
+
+                StringBuilder buf = new StringBuilder();
+                for (byte aMac : mac) {
+                    buf.append(String.format("%02X:", aMac));
+                }
+                if (buf.length() > 0) {
+                    buf.deleteCharAt(buf.length() - 1);
+                }
+                return buf.toString();
+            }
+        } catch (Exception ex) {
+        } // for now eat exceptions
+        return "";
+    }
+}
